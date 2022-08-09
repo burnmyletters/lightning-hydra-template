7b1599562232cd7d641eb24948f8e926b63fd329
Igor Krashenyi <reachingxforward@gmail.com>
2022-08-09 21:38:08 +0300

    add typing

--- src/models/components/timm_models.py
+++ src/models/components/timm_models.py
@@ -1,13 +1,16 @@
 import timm
+import torch
 from torch import nn
 
 
 class TimmModel(nn.Module):
-    def __init__(self, model_name, num_classes=1000, pretrained=True, in_chans=3):
+    def __init__(
+        self, model_name: str, num_classes: int = 1000, pretrained: bool = True, in_chans: int = 3
+    ) -> None:
         super().__init__()
         self.model = timm.create_model(
             model_name, pretrained=pretrained, in_chans=in_chans, num_classes=num_classes
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)

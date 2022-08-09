b09eec1afb9ef8e3a395f28b2dc3c8f053b72958
Igor Krashenyi <reachingxforward@gmail.com>
2022-08-09 21:36:04 +0300

    black

--- src/models/components/timm_models.py
+++ src/models/components/timm_models.py
@@ -1,11 +1,13 @@
-from torch import nn
 import timm
+from torch import nn
 
 
 class TimmModel(nn.Module):
     def __init__(self, model_name, num_classes=1000, pretrained=True, in_chans=3):
         super().__init__()
-        self.model = timm.create_model(model_name, pretrained=pretrained, in_chans=in_chans, num_classes=num_classes)
+        self.model = timm.create_model(
+            model_name, pretrained=pretrained, in_chans=in_chans, num_classes=num_classes
+        )
 
     def forward(self, x):
         return self.model(x)
